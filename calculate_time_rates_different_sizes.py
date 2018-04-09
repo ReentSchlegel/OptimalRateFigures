0b6c70fda7114f87fdeb5a7bd0bc46d290caec2c
ReentSchlegel <reent.schlegel@hotmail.de>
2018-04-09 16:11:48 +0200

    Formated comments

--- calculate_time_rates_different_sizes.py
+++ calculate_time_rates_different_sizes.py
@@ -5,12 +5,12 @@ import datetime
 
 
 
-filename = 'koptToptDifferentSize' + datetime.datetime.today().strftime("%I:%M%p_on_%B_%d_%Y")					# Name of file for the data to store
+filename = 'koptToptDifferentSize' + datetime.datetime.today().strftime("%I:%M%p_on_%B_%d_%Y")	# Name of file for the data to store
 
 
 n = 100										# Number of groups
 u = .1*n 									# Number of groups that can colaborate
-rs = np.array([0.05,0.1,1])*200000*100			# Dimension of A
+rs = np.array([0.05,0.1,1])*200000*100		# Dimension of A
 res = 100									# Number of evaluations
 mu = np.logspace(-4,4,res)					# res evenly spaced points on log axis 
 kopt = np.zeros((rs.size,mu.size))			# Allocate memory
